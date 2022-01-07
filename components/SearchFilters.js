c76b0a699361ce61ec562c12bf15961ecc336f27
mtkrated <57570477+mtkrated@users.noreply.github.com>
2022-01-07 22:34:17 +0200

    Finish Search Component

---
+++ components/SearchFilters.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from "react";
+import {Flex,Box,Select, Text, Spinner, Icon, Input} from "@chakra-ui/react";
+import {useRouter} from "next/router";
+import Image from "next/image"
+import {MdCancel} from "react-icons/md"
+
+const SearchFilters = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default SearchFilters
--- pages/search.js
+++ pages/search.js
@@ -3,22 +3,54 @@ import {useRouter} from "next/router";
 import Image from "next/image"
 import {Flex, Box, Text, Icon} from "@chakra-ui/react"
 import {BsFilter} from "react-icons/bs"
+import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import {baseUrl, fetchApi} from "../utils/fetchApi";
 
-const Search = () => {
+const Search = ({properties}) => {
     const [searchFilters, setSearchFilters] = useState(false)
     const router = useRouter()
 
-    return (
-        <Box>
+    return (<Box>
             <Flex onClick={() => setSearchFilters((prevState => !prevState))} cursor="pointer" bg="gray.100"
                   borderBottom="1px" borderColor="gray.200" padding="2" fontWeight="back" fontSize="l"
                   justifyContent="center" alignItems="center">
                 <Text>Search Property By Filter</Text>
                 <Icon paddingLeft="2" w="7" as={<BsFilter/>}/>
             </Flex>
-            {searchFilters && <SearchFilters/>>}
-        </Box>
-    )
+            {searchFilters && <SearchFilters/>}
+            <Text fontSize="2xl" p="4" fontWeight="bold">
+                Properties {router.query.purpose}
+            </Text>
+            <Flex flexWrap="wrap">
+                {properties.map((property) => <Property property={property} key={property.id}/>)}
+            </Flex>
+            {properties.length === 0 && (<Flex flexDirection="column" marginTop="5" marginBottom="5" alignItems="center"
+                                               justifyContent="center">
+                    <Image src={} alt="No result"/>
+                    <Text fontSize="2xl" marginTop="3">No Results Found</Text>
+                </Flex>)}
+        </Box>)
 }
 
 export default Search
+
+export async function getServersideProps({query}) {
+    const purpose = query.purpose || "for-rent"
+    const rentFrequency = query.rentFrequency || "yearly"
+    const minPrice = query.minPrice || "0"
+    const maxPrice = query.maxPrice || "10000"
+    const roomsMin = query.roomsMin || "0"
+    const bathsMin = query.bathsMin || "0"
+    const sort = query.sort || "price-desc"
+    const areaMax = query.areaMax || "30000"
+    const locationExternalIDs = query.locationExternalIDs || "5002"
+    const categoryExternalID = query.catergoryExternalID || "4  "
+
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&roomsMin=${roomsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&sort=${sort}&areaMax=${areaMax}`)
+    return {
+        props: {
+            properties: data?.hits
+        }
+    }
+}
